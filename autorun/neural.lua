059bcbaedbf4672a5e5a02d9a5ed99e2f60bd560
kepler155c <kepler155c@gmail.com>
2018-02-05 06:20:49 -0500

    robot wars

---
+++ autorun/neural.lua
@@ -0,0 +1,24 @@
+_G.requireInjector(_ENV)
+
+local GPS = require('gps')
+
+local device = _G.device
+
+if device.neuralInterface then
+  function device.neuralInterface.goTo(x, _, z)
+    local pt = GPS.locate(2)
+    if pt then
+      return pcall(function()
+        local gpt = {
+          x = x - pt.x,
+          y = 0,
+          z = z - pt.z,
+        }
+        gpt.x = math.min(math.max(gpt.x, -15), 15)
+        gpt.z = math.min(math.max(gpt.z, -15), 15)
+        return device.neuralInterface.walk(gpt.x, gpt.y, gpt.z)
+      end)
+    end
+    return false, 'No GPS'
+  end
+end
