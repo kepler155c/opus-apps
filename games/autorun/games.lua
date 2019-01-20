1de46ed8679e268a3688ad12b5b60ad6b95526a0
kepler155c@gmail.com <kepler155c@gmail.com>
2019-01-20 01:29:14 -0500

    use mounts for web apps

---
+++ games/autorun/games.lua
@@ -0,0 +1,6 @@
+fs.mount('packages/games/Othello.lua', 'urlfs', 'http://pastebin.com/raw/rMeh1Kag')
+fs.mount('packages/games/Pipes.lua', 'urlfs', 'https://pastebin.com/raw/skcs9x1s')
+fs.mount('packages/games/Strafe.lua', 'urlfs', 'https://pastebin.com/raw/jyDH7mLH')
+fs.mount('packages/games/Breakout.lua', 'urlfs', 'https://gist.github.com/LDDestroier/c7528d95bc0103545c2a/raw')
+fs.mount('packages/games/Minesweeper.lua', 'urlfs', 'https://pastebin.com/raw/nsKrHTbN')
+fs.mount('packages/games/Tron.lua', 'urlfs', 'https://raw.githubusercontent.com/LDDestroier/CC/master/tron.lua')
