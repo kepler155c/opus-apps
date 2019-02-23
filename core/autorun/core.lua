3d875890b01069338c8b927744ca3d259cbec479
kepler155c@gmail.com <kepler155c@gmail.com>
2019-02-23 08:17:04 -0500

    cleanup

--- core/autorun/core.lua
+++ core/autorun/core.lua
@@ -1,3 +1,6 @@
+if fs.exists('packages/core/lavaRefuel.lua') then fs.delete('packages/core/lavaRefuel.lua') end
+if fs.exists('packages/core/t.lua') then fs.delete('packages/core/t.lua') end
+
 _ENV.shell.setCompletionFunction("packages/core/edit.lua",
   function(shell, nIndex, sText)
     if nIndex == 1 then
