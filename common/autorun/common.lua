54bba18522f4821aa2e380f99e4d78ace38a104d
kepler155c@gmail.com <kepler155c@gmail.com>
2019-04-25 01:44:35 -0400

    hexEdit

--- common/autorun/common.lua
+++ common/autorun/common.lua
@@ -1,6 +1,10 @@
-_ENV.shell.setCompletionFunction("packages/common/edit.lua",
-  function(shell, nIndex, sText)
-    if nIndex == 1 then
-      return _G.fs.complete(sText, shell.dir(), true, false)
-    end
-  end)
+local c = function(shell, nIndex, sText)
+  if nIndex == 1 then
+    return _G.fs.complete(sText, shell.dir(), true, false)
+  end
+end
+
+_ENV.shell.setCompletionFunction("packages/common/edit.lua", c)
+_ENV.shell.setCompletionFunction("packages/common/hexedit.lua", c)
+
+fs.mount('packages/common/hexedit.lua', 'urlfs', 'https://pastebin.com/raw/Ds9ajsp4')
