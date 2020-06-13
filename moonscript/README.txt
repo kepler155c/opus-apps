27c7d2dd18a0eeb320db92bcdabda31a02a41352
kepler155c@gmail.com <kepler155c@gmail.com>
2020-06-13 12:19:13 -0600

    moonscript support fixes

--- moonscript/README.txt
+++
@@ -1,7 +0,0 @@
-moonscript must be run in compatibility mode:
-> compat moon T.moon
-> compat moonc T.moon
-
-moon and moonc were modified to allow relative paths:
-> cd /packages/moonscript
-> compat moonc T.moon
--- moonscript/autorun/startup.lua
+++ moonscript/autorun/startup.lua
@@ -1,7 +1,6 @@
 local Map = require('opus.map')
 
 local fs    = _G.fs
-local shell = _ENV.shell
 
 local commands = Map.transpose {
 	'packages/moonscript/moon',
@@ -16,21 +15,28 @@ local function compatEnv(source)
 	return env
 end
 
-shell.registerHandler(function(args, env)
-	if args[1]:match('(.+)%.moon$') then
+local function fix(env, args)
+	if #args > 0 then
+		args[1] = env.shell.resolve(args[1])
+	end
+	return args
+end
+
+_ENV.shell.registerHandler(function(env, command, args)
+	if command:match('(.+)%.moon$') then
 		return {
-			title = fs.getName(args[1]):match('([^%.]+)'),
+			title = fs.getName(command):match('([^%.]+)'),
 			path = 'packages/moonscript/moon',
-			args = args,
+			args = { env.shell.resolveProgram(command), table.unpack(args) },
 			load = loadfile,
 			env = compatEnv(env),
 		}
 	end
-	local command = shell.resolveProgram(args[1]) or ''
+	command = env.shell.resolveProgram(command) or ''
 	return commands[command] and {
 		title = fs.getName(command),
 		path = command,
-		args = { table.unpack(args, 2) },
+		args = fix(env, args),
 		load = loadfile,
 		env = compatEnv(env),
 	}
